284758bb89c313e6bb9bcfa676cf8a17b0680ba3
Greg Jopa <grjopa@gmail.com>
2017-06-07 16:58:58 -0700

    Update appointments page to work on mobile

--- app/js/index.js
+++ app/js/index.js
@@ -27,26 +27,42 @@ var app = {
   },
 
   createBookingWidget: function () {
+
+    if (document.location.pathname !== '/book-appointment') {
+      return;
+    }
+
     var $bookingWidget = $('#booking-widget');
     var $spinner = $bookingWidget.find('.loading-spinner');
+    var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      $spinner.hide();
+      var htmlMsg = '<p>Click the following button to book your appointment:</p>' +
+      '<a href="https://www.vagaro.com/beautyrefuge/about#online-scheduling" target="_blank" ' +
+      'rel="noopener" class="btn btn-primary btn-lg">Vagaro Beauty Refuge Website</a>';
+      $bookingWidget.find('p').html(htmlMsg);
+      return;
+    }
 
     var iframe = document.createElement('iframe');
     var $iframe = $(iframe);
 
-    iframe.src = 'https://www.vagaro.com/Users/BusinessWidget.aspx?' +
+    iframe.src = '//www.vagaro.com/Users/BusinessWidget.aspx?' +
       'enc=MMLjhIwJMcwFQhXLL7ifVFe2WD4QgtXLj5n/xLaCmsKxBWWioXABxhc5C5oT/' +
       'RU1KJfXeDHpMc0fJ+Qas1NOhIkWb1tBFTASY5r/0+axnVkRmW1YUSgt27P/cEwpJVjg#';
     iframe.scrolling = 'no';
     iframe.frameborder = '0';
     iframe.width = '780px';
     iframe.height = '600px';
-    iframe.style = 'display: none';
+    iframe.style.display = 'none';
     $bookingWidget.append(iframe);
 
     $iframe.on('load', function () {
       $spinner.hide();
-      $iframe.height(750).fadeIn();
+      $iframe.height(750).show();
     });
+
   }
 
 };
