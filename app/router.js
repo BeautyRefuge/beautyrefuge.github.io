a2ccd0d956038b876fa169506ab28dedaa7fb070
Greg Jopa <grjopa@gmail.com>
2017-06-27 09:01:25 -0700

    Add page descriptions

--- app/router.js
+++ app/router.js
@@ -4,21 +4,36 @@ const router = module.exports = new Router();
 const meta = require('./meta');
 
 router.get('/', async ctx => {
-  await ctx.render('home', { title: meta.home.title });
+  await ctx.render('home', {
+    title: meta.home.title,
+    description: meta.home.description
+  });
 });
 
 router.get('/services', async ctx => {
-  await ctx.render('services', { title: meta.services.title });
+  await ctx.render('services', {
+    title: meta.services.title,
+    description: meta.services.description
+  });
 });
 
 router.get('/gallery', async ctx => {
-  await ctx.render('gallery', { title: meta.gallery.title });
+  await ctx.render('gallery', {
+    title: meta.gallery.title,
+    description: meta.gallery.description
+  });
 });
 
 router.get('/contact', async ctx => {
-  await ctx.render('contact', { title: meta.contact.title });
+  await ctx.render('contact', {
+    title: meta.contact.title,
+    description: meta.contact.description
+  });
 });
 
 router.get('/book-appointment', async ctx => {
-  await ctx.render('appointment', { title: meta.bookAppointment.title });
+  await ctx.render('appointment', {
+    title: meta.bookAppointment.title,
+    description: meta.bookAppointment.description
+  });
 });
