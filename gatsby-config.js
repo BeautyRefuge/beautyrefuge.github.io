392cac50173b7edb8feaf5c509a118b13fe762f0
Greg Jopa <gjopa@cars.com>
2018-02-18 20:32:34 -0600

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-74100632-1',
+        head: false,
+      },
+    },
+  ],
 };
