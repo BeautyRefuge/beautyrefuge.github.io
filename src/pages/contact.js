a9dff8c48455db4c39bcdca4718e9b42e55c9f69
Greg Jopa <gjopa@cars.com>
2018-02-18 19:51:25 -0600

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import InstagramButton from '../components/InstagramButton';
+import Iframe from '../components/Iframe';
+
+export default () => (
+  <div>
+    <Helmet
+      title="Contact - Beauty Refuge Salon - Oakbrook Terrace, IL"
+      meta={[
+        {
+          name: 'description',
+          content:
+            'Get in touch with Beauty Refuge for an appointment. This salon is located in ' +
+            'Oakbrook Terrace, IL and specializes in color, extensions, and styling.',
+        },
+      ]}
+    />
+    <h1>Contact Us</h1>
+    <div className="row">
+      <div className="col-md-4">
+        <InstagramButton />
+        <p>
+          Thanks for your interest in Beauty Refuge. Please use this form to
+          schedule an appointment, ask a question, or just say hi!
+        </p>
+      </div>
+      <div
+        className="col-md-8"
+        style={{ minHeight: '400px', border: '1px solid #eee' }}
+      >
+        <Iframe
+          src="https://gregjopa.wufoo.com/embed/rzw3z4t182ay77/def/&header=hide"
+          width="100%"
+          height="500px"
+        />
+      </div>
+    </div>
+  </div>
+);
