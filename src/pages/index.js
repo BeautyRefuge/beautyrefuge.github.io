61e0ef0664917e3b5088aa25530a68885fb0b820
Greg Jopa <gjopa@cars.com>
2018-02-18 19:44:15 -0600

    Add homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const IndexPage = () => (
+import Slideshow from '../components/Slideshow';
+import Expertise from '../components/Expertise';
+import Reviews from '../components/Reviews';
+
+export default () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="row" style={{ marginTop: '20px' }}>
+      <div className="col-md-5">
+        <h2>Beauty Refuge</h2>
+        <p>
+          A single chair suite, with rustic boho style and a comfortable
+          atmosphere.
+        </p>
+        <div style={{ margin: '20px 0' }}>
+          Beauty Refuge Salon<br />
+          1909 Meyers Rd, Suite 112<br />
+          Oakbrook Terrace, IL 60181<br />
+          (630) 600-8482 - texting preferred
+        </div>
+        <p>
+          <a
+            target="_blank"
+            rel="noopener"
+            href="https://goo.gl/maps/GiwWPgfJjNP2"
+          >
+            Click here for Directions
+          </a>
+        </p>
+      </div>
+      <div className="col-lg-7">
+        <Slideshow />
+      </div>
+    </div>
+
+    <Expertise />
+    <Reviews />
   </div>
 );
-
-export default IndexPage;
